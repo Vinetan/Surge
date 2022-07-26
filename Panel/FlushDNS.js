5dc7d070a480ac2c111350206e317e6aa11d23b2
Vinetan <80163603+Vinetan@users.noreply.github.com>
2022-07-26 13:54:30 +0800

    Update FlushDNS.js

--- Panel/FlushDNS.js
+++ Panel/FlushDNS.js
@@ -1,25 +1,15 @@
 /*
-#!name=Flush DNS
-#!desc=Flush DNS by @zZPiglet
-#!system=ios
-
-[Panel]
-flushDNS = script-name=flushDNS,update-interval=600
-
 [Script]
-flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/Vinetan/Surge/main/Panel/FlushDNS.js,argument=title=DNS FLush&icon=arrow.triangle.2.circlepath.circle&color=#0089A7&server=false
-
-[Script]
-flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/Vinetan/Surge/main/Panel/FlushDNS.js
+flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
 // use "title" or "icon" or "color" or "server" in "argument":
-// flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/Vinetan/Surge/main/Panel/FlushDNS.js,argument=title=DNS FLush&icon=arrow.clockwise&color=#3d3d5b&server=false
+// flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js,argument=title=DNS FLush&icon=arrow.clockwise&color=#3d3d5b&server=true
 
 [Panel]
 flushDNS = script-name=flushDNS,update-interval=600
 */
 
 !(async () => {
-    let panel = { title: "𝗙𝗹𝘂𝘀𝗵 𝗗𝗡𝗦" },
+    let panel = { title: "𝗗𝗡𝗦 𝗦𝗲𝗿𝘃𝗲𝗿" },
         showServer = true,
         dnsCache;
     if (typeof $argument != "undefined") {
@@ -33,9 +23,14 @@ flushDNS = script-name=flushDNS,update-interval=600
         dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
         dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
     }
+    if (console.log(dnsCache.indexOf("adguard-dns.com/dns-query") != -1)) {
+        dnsCache = Availabal
+    } else {
+        dnsCache = Unavailabal
+    }
     if ($trigger == "button") await httpAPI("/v1/dns/flush");
-    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
-    panel.content = `𝗱𝗲𝗹𝗮𝘆 : ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`;
+    // let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    panel.content = `DoH Server is ${dnsCache}`;
     $done(panel);
 })();
 
