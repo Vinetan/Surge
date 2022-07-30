a48a670cef6504c2eef209777b92900fa22feb09
Vinetan <80163603+Vinetan@users.noreply.github.com>
2022-07-30 10:54:59 +0800

    Create Anti_IP_Info.js

---
+++ Panel/Anti_IP_Info.js
@@ -0,0 +1,51 @@
+/**
+* 感谢@fishingworld大佬的智慧，参考文件地址：https://github.com/fishingworld/something/blob/main/PanelScripts/net_info.js
+*/
+const { wifi, v4, v6 } = $network;
+
+;(async () => {
+
+
+
+  let params = getParams($argument)
+  //获取根节点名
+  let proxy = await httpAPI("/v1/policy_groups");
+  let allGroup = [];
+  for (var key in proxy){
+     allGroup.push(key)
+      }
+  let group = params.group
+  let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(group)+"")).policy;
+  while(allGroup.includes(rootName)==true){
+    rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
+  }
+  
+  $httpClient.get('http://ip-api.com/json', function (error, response, data) {
+      const jsonData = JSON.parse(data);
+      $done({
+        title: rootName,
+        content: `𝗬𝗼𝘂𝗿 𝗔𝗻𝘁𝗶 𝗜𝗣 𝗜𝗻 ${jsonData.city}, ${jsonData.countryCode}`,
+        icon: mappin.slash.circle,
+        'icon-color': wifi.ssid ? '#007aff' : '#75d671'
+      });
+    });
+  
+  })();
+  
+  
+  function httpAPI(path = "", method = "GET", body = null) {
+      return new Promise((resolve) => {
+          $httpAPI(method, path, body, (result) => {
+              resolve(result);
+          });
+      });
+  };
+  
+  function getParams(param) {
+    return Object.fromEntries(
+      $argument
+        .split("&")
+        .map((item) => item.split("="))
+        .map(([k, v]) => [k, decodeURIComponent(v)])
+    );
+  }
